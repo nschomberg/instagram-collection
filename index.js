07ce980d03d071c4c44c753a7141a643107f7de1
Nico <nicolas.schomberg@gmail.com>
2016-05-08 23:08:53 -0700

    skeleton

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config.json');
 
-var database = require('./lib/database-service');
+mongoose.connect(config.mongodbUrl);
+//var database = require('./lib/database-service');
+//
+var instagram = require('./lib/instagram-api');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,9 +17,15 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+	response.render('pages/index');
+});
+
+app.get('/api/collections', function(request, response) {
+	response.json({
+		message: 'success'
+	});
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+	console.log('Node app is running on port', app.get('port'));
 });
