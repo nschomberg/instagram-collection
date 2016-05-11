3310d21055c8e9a9872196cb7410c7693b0d5d70
Nico <nicolas.schomberg@gmail.com>
2016-05-10 23:45:25 -0700

    Stringing together API and DB

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
@@ -21,9 +23,43 @@ app.get('/', function(request, response) {
 });
 
 app.get('/api/collections', function(request, response) {
-	response.json({
-		message: 'success'
+	console.dir(request.query);
+
+	var Collection = require('./lib/models/collection');
+
+	Collection.find({}, 'tag', function (err, collections) {
+	  if (err) return err;
+		response.json({
+			message: collections
+		});
 	});
+});
+
+app.get('/api/content', function(request, response) {
+	console.dir(request.query);
+
+	var Collection = require('./lib/models/content');
+
+	Collection.find({"_collection" : request.query.collection}, 'link created_time _collection', function (err, content) {
+	  if (err) return err;
+		response.json({
+			message: content
+		});
+	});
+});
+
+app.post('/api/create', function(request, response) {
+	console.dir(request.query);
+
+	var instagram = require('./lib/instagram-api');
+
+	instagram
+		.collect(request.query.tag)
+		.then(function(){
+			response.json({
+				message: 'request'
+			});
+		});
 });
 
 app.listen(app.get('port'), function() {
